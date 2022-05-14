5c6da65bb2903850bcdeb79b74baf6e008be4f61
Gabriel Faria <gabriel.selu@gmail.com>
2022-05-14 18:30:16 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
-import Navbar from "./components/navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Listing from './pages/Listing/index';
+import Form from './pages/Form/index';
+import Navbar from "./components/Navbar/index";
 
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
